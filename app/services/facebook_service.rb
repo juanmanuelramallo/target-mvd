7eed4348e6ecc29edf3c322c9e6d56c05267ad2a
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-04-30 14:33:22 -0300

    [1] Facebook login
    - Resolves: https://trello.com/c/8STuNXZd/1-2-as-a-user-i-should-be-able-to-login-with-facebook-so-i-can-register-faster

---
+++ app/services/facebook_service.rb
@@ -0,0 +1,50 @@
+require 'net/http'
+
+class FacebookService
+  attr_reader :access_token
+
+  BASE_URL = "https://graph.facebook.com".freeze
+
+  def self.call!(access_token)
+    new(access_token).user!
+  end
+
+  def initialize(access_token)
+    @access_token = access_token
+  end
+
+  def user!
+    raise FacebookError.new(error_message) if error?
+
+    User.where(uid: response['email']).first_or_create! do |user|
+      user.email = response['email']
+      user.password = Devise.friendly_token[0, 20]
+      user.provider = 'facebook'
+      user.skip_confirmation!
+    end
+  end
+
+  private
+
+  def error?
+    response["error"].present?
+  end
+
+  def error_message
+    response["error"]["message"]
+  end
+
+  def response
+    return @response if @response
+
+    req = Net::HTTP::Get.new url.request_uri
+    http = Net::HTTP.new url.host, url.port
+    http.use_ssl = true
+    res = http.request req
+    @response = JSON.parse res.body
+  end
+
+  def url
+    @url ||= URI.parse "#{BASE_URL}/me?fields=email&access_token=#{access_token}"
+  end
+end
