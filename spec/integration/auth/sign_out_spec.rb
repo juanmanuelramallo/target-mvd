091047726b854e0c4f06cd2aab6d1b0d2d10c56b
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-05-02 12:12:51 -0300

    [16] User logout
    - Resolves: https://trello.com/c/CnXNHcsx/16-2-as-a-user-i-should-be-able-to-logout-so-i-can-no-longer-be-logged-in

---
+++ spec/integration/auth/sign_out_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'Signing user in', type: :request do
+  let(:access_token) { response.header['access-token'] }
+  let(:client) { response.header['client'] }
+  let(:uid) { response.header['uid'] }
+  let(:headers) do
+    {
+      'access-token': access_token,
+      client: client,
+      uid: uid
+    }
+  end
+
+  subject do
+    delete '/auth/sign_out', headers: headers
+    response
+  end
+
+  context 'logged in user' do
+    let(:user) { create :user }
+
+    before do
+      post '/auth/sign_in', params: { email: user.email, password: 'P@55word' }
+    end
+
+    it 'should allow me to log out' do
+      expect(subject).to have_http_status(:success)
+    end
+  end
+
+  context 'already logged out user' do
+    let(:user) { create :user }
+    let(:token) { user.create_token }
+    let(:access_token) { token[1] }
+    let(:client) { token[0] }
+    let(:uid) { user.email }
+
+    it "shouldn't allow me to log out again" do
+      expect(subject).to have_http_status(:not_found)
+    end
+  end
+end
