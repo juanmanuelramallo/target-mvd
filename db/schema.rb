ef34602828ed582e5fdecd17413574094ffbd041
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-05-02 12:35:16 -0300

    [10] Reset password (#8)
    - Resolves: https://trello.com/c/w1V8Yl7t/10-3-as-a-user-i-should-be-able-to-reset-my-password-so-i-can-regain-access-to-my-account-if-i-forgot-it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_429_171_550) do
+ActiveRecord::Schema.define(version: 20_190_502_152_424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20_190_429_171_550) do
     t.string 'provider', default: 'email', null: false
     t.string 'uid', default: '', null: false
     t.json 'tokens'
+    t.boolean 'allow_password_change', default: false, null: false
     t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
---
+++ spec/integration/auth/reset_password_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Reset password', type: :request do
+  let(:params) do
+    {
+      email: email,
+      redirect_url: '/reset-password'
+    }
+  end
+
+  subject do
+    post '/auth/password', params: params
+    response
+  end
+
+  context 'valid user' do
+    let(:user) { create :user }
+    let(:email) { user.email }
+
+    it 'should send me an email' do
+      expect { subject }.to change { ActionMailer::Base.deliveries.count }.by 1
+    end
+  end
+
+  context 'invalid user' do
+    let(:email) { 'fake-user@example.com' }
+
+    it "shouldn't send me an email" do
+      expect { subject }.to change { ActionMailer::Base.deliveries.count }.by 0
+    end
+  end
+end
