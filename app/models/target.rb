ff52b5d4e55259e645d0b8650fc3c7130a6170c1
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-05-06 11:05:22 -0300

    [6] Maximum targets

--- app/models/target.rb
+++ app/models/target.rb
@@ -4,4 +4,14 @@ class Target < ApplicationRecord
 
   validates :title, :lat, :lng, presence: true
   validates :area_length, numericality: { greater_than: 0 }
+  validate :user_must_have_less_than_maximum_targets
+
+  private
+
+  def user_must_have_less_than_maximum_targets
+    return unless user && user.targets.count >= User::MAXIMUM_TARGETS
+
+    errors.add :base,
+               I18n.t('.user_must_have_less_than_maximum_targets', max: User::MAXIMUM_TARGETS)
+  end
 end
