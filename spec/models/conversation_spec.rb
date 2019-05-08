c6652625f75027b9435eda28b271f24800ce1b32
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-05-08 14:34:40 -0300

    [12] Conversations with compatible targets
    - Part 3
    - Adds the validation to check if an initiator is creating a conversation against a compatible target

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -3,9 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Conversation, type: :model do
+  subject { create :conversation }
+
   describe 'associations' do
     it { should have_many(:messages).dependent(:destroy) }
     it { should belong_to(:target) }
     it { should belong_to(:initiator) }
+  end
+
+  describe 'validations' do
+    subject { create :conversation }
+
+    context 'should have a target compatible with the initiator' do
+      context 'conversation valid' do
+        it 'should be valid' do
+          expect(subject.valid?).to be true
+        end
+      end
+
+      context 'conversation invalid' do
+        subject { build :conversation, target: target, initiator: target.user }
+        let(:target) { create :target }
+
+        it 'should be invalid' do
+          expect(subject.valid?).to be false
+        end
+      end
+    end
   end
 end
