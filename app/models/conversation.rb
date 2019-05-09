ec3508ed912893c26c617c7550c415a473adc6b3
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-05-09 10:56:29 -0300

    [14] Unread conversations fixes
    - Using `is_expected.to` instead of `should`
    - Removing unneeded option `inverse_of` in conversation model
    - Removing unneeded scope in conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Conversation < ApplicationRecord
-  has_many :messages, -> { order :created_at }, dependent: :destroy, inverse_of: :conversation
+  has_many :messages, dependent: :destroy
 
   belongs_to :target
   belongs_to :initiator, class_name: 'User'
--- spec/models/target_spec.rb
+++ spec/models/target_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe Target, type: :model do
   subject { build :target }
 
   describe 'associations' do
-    it { should belong_to(:topic) }
-    it { should belong_to(:user) }
+    it { is_expected.to belong_to(:topic) }
+    it { is_expected.to belong_to(:user) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:lat) }
-    it { should validate_presence_of(:lng) }
-    it { should validate_numericality_of(:area_length).is_greater_than(0) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:lat) }
+    it { is_expected.to validate_presence_of(:lng) }
+    it { is_expected.to validate_numericality_of(:area_length).is_greater_than(0) }
 
     context 'user must have less than 10 targets' do
       let(:target) { build :target, user: user }
