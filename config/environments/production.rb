06c395609d49a65feb343d19c3f4c452c8336d84
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-05-10 13:03:33 -0300

    [Production] Adding action cable configurations for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://jm-target-mvd.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['http://localhost:3000']
 end
