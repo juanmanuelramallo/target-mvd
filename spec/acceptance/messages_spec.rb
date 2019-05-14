34b034572f2c8ecb48e5e28dadacef3234046104
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-05-14 12:02:16 -0300

    [13] Messages create action (#31)
    * [DOCS] Generating docs for messages create
    - Using `save!` in controller

---
+++ spec/acceptance/messages_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource 'Messages' do
+  header 'access-token', :access_token_header
+  header 'client', :client_header
+  header 'uid', :uid_header
+
+  let(:conversation) { create :conversation }
+  let(:message) { build :message, conversation: conversation, user: user }
+  let(:user) { conversation.initiator }
+
+  route '/conversations/:conversation_id/messages', 'Messages collection' do
+    let(:conversation_id) { conversation.id }
+
+    post 'Create' do
+      with_options scope: :message, required: true do
+        response_field :text, 'Text of the message'
+        response_field :created_at, 'Created at timestamp'
+        response_field :user_id, 'Sender of the message'
+      end
+
+      with_options scope: :message, required: true do
+        attribute :text, 'Message to send'
+      end
+
+      let(:text) { message.text }
+
+      example 'Ok' do
+        do_request
+
+        expect(status).to eq 201
+      end
+    end
+  end
+end
