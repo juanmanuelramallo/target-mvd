8a816a0950346d6a7d8a858184440333b36331f0
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-05-17 17:06:15 -0300

    [38] Using ticketing system to authorize websockets connections

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -5,26 +5,15 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      self.current_user = CableTicketValidationService.call!(ticket)
+    rescue TicketInvalidError
+      reject_unauthorized_connection
     end
 
     private
 
-    def find_verified_user
-      uid = headers['uid']
-      token = headers['access-token']
-      client = headers['client']
-      user = User.find_by(uid: uid)
-
-      if user&.valid_token?(token, client)
-        user
-      else
-        reject_unauthorized_connection
-      end
-    end
-
-    def headers
-      @headers ||= request.headers
+    def ticket
+      request.query_parameters[:ticket]
     end
   end
 end
--- spec/channels/connection_spec.rb
+++ spec/channels/connection_spec.rb
@@ -3,28 +3,51 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationCable::Connection, type: :channel do
+  let(:created_at) { Time.now.in_time_zone }
+  let(:ip) { Faker::Internet.public_ip_v4_address }
+  let(:ticket) do
+    Ticket.new(user_id: user.id, ip: ip,
+               created_at: created_at)
+  end
+  let(:ticket_value) { ticket.encode }
+  let(:user) { create :user }
+
   subject do
-    connect '/cable', headers: headers
+    connect "/cable?ticket=#{ticket_value}", headers: headers
     connection
   end
 
-  context 'with valid user' do
-    let(:user) { create :user }
-
+  context 'with valid ticket' do
     it 'successfully connects' do
       expect(subject.current_user).to eq user
     end
+
+    it 'deletes the key in the cache' do
+      Rails.cache.fetch(ticket) { ticket }
+      expect { subject }.to change { Rails.cache.fetch(ticket) }.to nil
+    end
   end
 
-  context 'with anonymous user' do
-    let(:headers) do
-      {
-        'access-token': 'fake_token',
-        'token-type': 'Bearer',
-        client: 'fake_client',
-        expiry: '123456',
-        uid: 'fake_uid@example.com'
-      }
+  context 'with no ticket' do
+    let(:ticket_value) { nil }
+
+    it 'rejects the connection' do
+      expect { subject }.to have_rejected_connection
+    end
+  end
+
+  context 'with an old ticket' do
+    let(:created_at) { Time.now.in_time_zone - 10.minutes }
+
+    it 'rejects the connection' do
+      expect { subject }.to have_rejected_connection
+    end
+  end
+
+  context 'with a ticket from a different ip' do
+    before do
+      Ticket.new(user_id: user.id, ip: Faker::Internet.public_ip_v4_address,
+                 created_at: created_at).encode
     end
 
     it 'rejects the connection' do
