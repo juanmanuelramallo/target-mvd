f0248c0c156868235f08069ecb05de926d4da0bd
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2019-05-24 11:49:12 -0300

    [32] Conversations index pagination (#64)
    - Added pagy gem
    - Using pagination links from json:api adapter https://github.com/rails-api/active_model_serializers/blob/0-10-stable/docs/howto/add_pagination_links.md

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
   include Concerns::CommonErrorsConcern
   include Concerns::IncludeConcern
+  include Concerns::PaginationConcern
+  include DeviseTokenAuth::Concerns::SetUserByToken
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
